07af6b283a82ee07cd6d746cff875fb97e4b414b
ErrBrush <errbrushed@gmail.com>
2026-03-07 13:45:28 -0700

    Update run.sh

--- content/contest/run.sh
+++ content/contest/run.sh
@@ -1,10 +1,13 @@
 set -e
 clear; clear
 f=$(ls -t -1 *.cc | head -n1)
-g++ -g -std=gnu++23 -static -Wall -W -Wconversion -Wshadow -fsanitize=undefined -o sol $f
+g++ -g -std=gnu++23 -Wall -W -Wconversion -Wshadow -fsanitize=address,undefined -o sol $f
 for i in *.in; do
     echo --- $i
     ./sol < $i >k 2>g
     diff k ${i%.in}.[ao]??
 done
 echo PASS
+if [[ "$1" == "yolo" ]]; then
+    ../submit
+fi
